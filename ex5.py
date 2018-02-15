150498d9ed0674822a67080b8bca8683fd5e7187
RAGUPATHI537 <36394314+RAGUPATHI537@users.noreply.github.com>
2018-02-15 10:57:22 +0530

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,16 @@
+a=input()
+
+b=input()
+
+c=input()
+
+if (a>b and a>c):
+  
+  print("a is largest num")
+
+elif (b>a and b>c):
+    
+print("b is largest num")
+
+else:
+    print ("c is largest num")
