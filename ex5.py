02db886e7660498cc2880ff14bfd88e9237d8033
RAGUPATHI537 <36394314+RAGUPATHI537@users.noreply.github.com>
2018-02-15 10:59:41 +0530

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -6,11 +6,11 @@ c=input()
 
 if (a>b and a>c):
   
-  print("a is largest num")
+  print("a is largest num",a)
 
 elif (b>a and b>c):
     
-print("b is largest num")
+print("b is largest num",b)
 
 else:
-    print ("c is largest num")
+    print ("c is largest num",c)
